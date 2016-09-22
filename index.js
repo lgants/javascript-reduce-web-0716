d901a47b712fc7fdc286f288a2ad14d395de6239
lgants <lgants@gmail.com>
2016-09-22 10:02:24 -0400

    javascript reduce web complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
+
+console.log(reduce(couponLocations, couponCounter, 0)); // prints 15
+console.log(reduce(couponLocations, couponCounter, 3)); // prints 18
+console.log(couponLocations.reduce(couponCounter, 0)); // also prints 15!
